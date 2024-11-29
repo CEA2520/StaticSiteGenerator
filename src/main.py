94d814853ceb0f53f3290466a2444da910e8ca2b
CEA2520 <abram.cheyenne@outlook.com>
2024-11-29 14:02:02 -0800

    added main() to main.py with test case. added classes to textnode.py with eq and repr methods

--- src/main.py
+++ src/main.py
@@ -1,1 +1,9 @@
-print("Hello World")
+from textnode import *
+
+def main():
+ 
+   # Create a test node with some dummy values
+   node = TextNode("test text", TextType.BOLD_TEXT)
+   print(node)
+
+main()
